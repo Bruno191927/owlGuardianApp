2e703255bc8c2bbd0921659d838dbf0e900ae1ee
KVeramendi <1611619@utp.edu.pe>
2022-11-11 23:51:39 -0500

    feat: :art: Agregar controladores
    Agregando contorlador para la página de Login

--- lib/presentation/views/pages/login/login_page.dart
+++ lib/presentation/views/pages/login/login_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:hackathon_app/presentation/core/colors/app_colors.dart';
 import 'package:hackathon_app/presentation/core/shared_widgets/custom_elevatedbutton_widget.dart';
 import 'package:hackathon_app/presentation/core/shared_widgets/custom_textfieldform_widget.dart';
 import 'package:hackathon_app/presentation/views/pages/home/home_page.dart';
+import 'package:hackathon_app/presentation/views/pages/login/login_controller.dart';
 import 'package:hackathon_app/presentation/views/pages/register/register_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   static const routeName = "login";
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final LoginController _loginController = LoginController();
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance?.addPostFrameCallback((timeStamp) {
+      _loginController.init(context: context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -37,17 +54,17 @@ class LoginPage extends StatelessWidget {
                     icon: Icons.email_outlined,
                     inputType: TextInputType.emailAddress),
                 const SizedBox(height: 20.0),
-                CustomTextFieldForm(
+                const CustomTextFieldForm(
                   text: 'Contraseña',
                   icon: Icons.password_outlined,
                   inputType: TextInputType.visiblePassword,
                   obscureText: true,
-                  onTap: () {
-                    Navigator.pushNamed(context, HomePage.routeName);
-                  },
                 ),
                 const SizedBox(height: 40.0),
-                const CustomElevatedButton(text: 'Iniciar sesión'),
+                CustomElevatedButton(
+                  text: 'Iniciar sesión',
+                  onPressed: () => _loginController.submit(),
+                ),
                 TextButton(
                     onPressed: () {},
                     child: Text('¿Olvidaste la contraseña?',
@@ -57,9 +74,7 @@ class LoginPage extends StatelessWidget {
                   Text('¿Aún no tienes una cuenta?',
                       style: TextStyle(color: AppColors.text.fieldForm)),
                   TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, RegisterPage.routeName);
-                      },
+                      onPressed: () => _loginController.register(),
                       child: Text('Registrarse',
                           style: TextStyle(color: AppColors.text.cyan)))
                 ])
