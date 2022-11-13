14992a5bd7ab1b349d4ff3d01f429ee7fda851eb
KVeramendi <1611619@utp.edu.pe>
2022-11-12 23:19:15 -0500

    feat: :art: Incident Page
    Se agregó el campo TimePicker

--- lib/presentation/views/pages/incidence/incidence_page.dart
+++ lib/presentation/views/pages/incidence/incidence_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon_app/presentation/core/colors/app_colors.dart';
+import 'package:hackathon_app/presentation/core/shared_widgets/custom_datetimebutton_widget.dart';
 import 'package:hackathon_app/presentation/core/shared_widgets/custom_dropdownbutton_widget.dart';
 import 'package:hackathon_app/presentation/core/shared_widgets/custom_elevatedbutton_widget.dart';
 import 'package:hackathon_app/presentation/core/shared_widgets/custom_textfieldform_widget.dart';
+import 'package:hackathon_app/presentation/views/pages/incidence/incidence_controller.dart';
 
 class IncidencePage extends StatefulWidget {
   static const routeName = "incidence";
@@ -13,13 +15,21 @@ class IncidencePage extends StatefulWidget {
 }
 
 class _IncidencePageState extends State<IncidencePage> {
+  final IncidenceController _incidenceController = IncidenceController();
+  late DateTime _datetime;
   final _categoryItems = [
-    'Muy ligera',
-    'Ligera',
-    'Regular',
-    'Activa',
-    'Muy Activa'
+    'Denuncia pública',
+    'Disturbios',
+    'Homicidios',
+    'Obras inconclusas'
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _incidenceController.init(context: context);
+    _datetime = DateTime.now();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,14 +66,36 @@ class _IncidencePageState extends State<IncidencePage> {
                   borderColor: AppColors.orange,
                 ),
                 const SizedBox(height: 20.0),
-                CustomDropDownButton(items: _categoryItems),
+                CustomDropDownButton(
+                  items: _categoryItems,
+                  text: "Catorgías",
+                ),
+                const SizedBox(height: 20.0),
+                CustomDateTimeButton(
+                    initialValue:
+                        "${_datetime.year}-${_datetime.month}-${_datetime.day}",
+                    text: "Fecha",
+                    onTap: () async {
+                      final _curreDateTime = await _incidenceController
+                          .pickDate(context, _datetime);
+                      setState(() => _datetime = _curreDateTime!);
+                    }),
+                const SizedBox(height: 20.0),
+                CustomTextFieldForm(
+                  text: "Fecha",
+                  icon: Icons.date_range_outlined,
+                  inputType: TextInputType.datetime,
+                  textColor: AppColors.black,
+                  textFormColor: AppColors.black,
+                  borderColor: AppColors.orange,
+                ),
                 const SizedBox(height: 20.0),
                 CustomElevatedButton(
                   text: "Agregar",
                   onPressed: () {},
                   buttonColor: AppColors.orange,
                   textColor: AppColors.white,
-                )
+                ),
               ],
             ),
           )),
